9dc614e878616718f733bf94ded653de7b5fd2cb
Ricardo Isidro <ricardo.isidro33@gmail.com>
2022-11-07 12:35:10 +0000

    index changes

--- db/index.js
+++ db/index.js
@@ -13,6 +13,7 @@ mongoose
   .then((x) => {
     const databaseName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+    
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const Team = require('../models/Team.model')
+const Player = require('../models/Player.model')
+const isLoggedOut = require("../middleware/isLoggedOut");
+const isLoggedIn = require("../middleware/isLoggedIn");
+
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", isLoggedIn, async (req, res, next) => {
+  try {
+    let teams = await Team.find();
+  console.log(teams.name)
+  res.render("index", {teams});
+  } catch (error) {
+    console.log(error)
+      next(error);
+  }
+  
 });
 
+
+
+router.get("/team/:id", (req,res,next) =>{
+  
+})
+
+/* 
+router.get('/teams', async (req,res,next) =>{
+  try{
+      let teams = await Team.find();
+      console.log(teams.name)
+      res.render("index", {teams})
+  } catch(error){
+      console.log(error)
+      next(error);
+  }
+
+}) */
+
+
+
 module.exports = router;
+ 
